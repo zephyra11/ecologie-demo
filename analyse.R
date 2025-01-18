def82736ba5ee781cd315c476258d916e1b3f60f
Laurence Legault <195580994+zephyra11@users.noreply.github.com>
2025-01-18 15:28:47 -0500

    ajout de alpha

--- analyse.R
+++ analyse.R
@@ -2,9 +2,12 @@
 library(tidyverse)
 library(ratdat)
 #graphique
-ggplot(data = complete_old, aes(x=weight, y=hindfoot_length)) + geom_point(color="red")
+ggplot(data = complete_old, aes(x=weight, y=hindfoot_length)) + geom_point(color="red", alpha=0.1)
 
 #commande pour git
 #git add/ git commit -m / git status / git log
 #git add analyse.R / git commit -m "ajout de la couleur/ 
 
+#faut tjs faire save le fichier, add dans terminal, commit
+#onglet à droite en haut, cliquer sur le nom (case) cliquer sur commit, ecrire le message
+#history pour voir les modif
