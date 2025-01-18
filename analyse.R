06019ec5329e2a56ea848ab235b4dc9e59917a7a
Laurence Legault <195580994+zephyra11@users.noreply.github.com>
2025-01-18 15:24:37 -0500

    changer la couleur en rouge

--- analyse.R
+++ analyse.R
@@ -2,4 +2,9 @@
 library(tidyverse)
 library(ratdat)
 #graphique
-ggplot(data = complete_old, aes(x=weight,y=hindfoot_lenght))+ geom_point
+ggplot(data = complete_old, aes(x=weight, y=hindfoot_length)) + geom_point(color="red")
+
+#commande pour git
+#git add/ git commit -m / git status / git log
+#git add analyse.R / git commit -m "ajout de la couleur/ 
+
